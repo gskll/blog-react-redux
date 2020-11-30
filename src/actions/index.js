af20b11ed5528a84f626d7cbc8e54bd83278ffa3
Andrew <gskll@protonmail.com>
2020-11-30 16:40:24 +0100

    feat: add action to fetch json data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
-export const fetchPosts = () => {
-  return { type: "FETCH_POSTS", payload: { posts: "POSTS" } };
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response,
+  });
 };
